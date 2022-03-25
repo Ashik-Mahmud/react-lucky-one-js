ff41aef70f6252d34c50a56e532cf11a25ca3ce7
ashikmahmud <ashikmamud187@gmail.com>
2022-03-26 00:42:43 +0600

    added header section of this project lovely

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdOutlineMapsHomeWork } from "react-icons/md";
 import "./Header.css";
 const Header = () => {
   return (
@@ -6,7 +7,10 @@ const Header = () => {
       <header id="header">
         <div className="container">
           <div className="header-content">
-            <h1>Real State App</h1>
+            <h1>
+              <MdOutlineMapsHomeWork />
+              Real <span className="colorize">State</span>{" "}
+            </h1>
             <p>Get you dream house from here </p>
           </div>
         </div>
