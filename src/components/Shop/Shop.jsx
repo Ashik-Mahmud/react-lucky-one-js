ab7c909469e8c778845be0fd513cda8283256d69
ashikmahmud <ashikmamud187@gmail.com>
2022-03-26 03:01:57 +0600

    added new features picking random items with loading system

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 import CartItem from "../CartItem/CartItem";
 import Product from "../Product/Product";
 import { UseStorage } from "../Storage/Storage";
@@ -6,16 +7,34 @@ import "./Shop.css";
 const Shop = ({ products }) => {
   const [savedItem, setSavedItem] = useState([]);
   const [error, setError] = useState("");
+  const [cartItems, setCartItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setSavedItem(UseStorage());
   }, []);
 
-  const addedItems = products.filter((product) =>
-    savedItem.includes(product.id)
-  );
+  useEffect(() => {
+    const addedItems = products.filter((product) =>
+      savedItem.includes(product.id)
+    );
+    setCartItems(addedItems);
+  }, [products, savedItem]);
 
-  setTimeout(() => setError(false), 10000);
+  /* handle pick best one function */
+  const handlePickBestOne = () => {
+    setLoading(true);
+    const items = UseStorage();
+    const randomNum = Math.floor(Math.random() * items.length);
+    const cartItem = products.find(
+      (product) => product.id === items[randomNum]
+    );
+
+    setTimeout(() => {
+      setCartItems([cartItem]);
+      setLoading(false);
+    }, 5000);
+  };
 
   return (
     <>
@@ -33,16 +52,40 @@ const Shop = ({ products }) => {
               ))}
             </div>
             <div className="shop-dashboard">
-              <h3>Items Summery</h3>
-              <div className="cart-container">
-                {addedItems.map((item) => (
-                  <CartItem key={item.id} item={item} />
-                ))}
-              </div>
-              <div className="carts-btn">
-                <button className="draw-cart">Pick Best One</button>
-                <button className="clear-cart">Clear Cart</button>
-              </div>
+              {savedItem.length > 0 ? (
+                <>
+                  <h3>Items Summery</h3>
+                  <div className="cart-container">
+                    {!loading ? (
+                      cartItems.map((item) => (
+                        <CartItem key={item.id} item={item} />
+                      ))
+                    ) : (
+                      <div className="waiting">
+                        <img
+                          src="https://i.pinimg.com/originals/2b/02/15/2b02159fee58d573c079ad5212d56b63.gif"
+                          alt="loader"
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div className="carts-btn">
+                    <button className="draw-cart" onClick={handlePickBestOne}>
+                      Pick Best One
+                    </button>
+                    <button className="clear-cart">Clear Cart</button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="empty-cart-items">
+                    <h1>
+                      <MdOutlineRemoveShoppingCart />
+                    </h1>
+                    <h3>No Items Found.</h3>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
