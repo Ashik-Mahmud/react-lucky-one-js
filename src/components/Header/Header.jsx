58897a7ec6f0144d8b51a174f5ebeb0c9d07b45d
ashikmahmud <ashikmamud187@gmail.com>
2022-03-26 00:36:52 +0600

    create fake data json file my own yahhh

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <>
+      <header id="header">
+        <div className="container">
+          <div className="header-content">
+            <h1>Real State App</h1>
+            <p>Get you dream house from here </p>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
